94c0271ad86f056113d6b34d9cee97fb5faf95d1
jerlibgnzlz <jerlibgv@gmail.com>
2023-06-21 22:04:58 -0300

    widget de formulario

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -5,5 +5,4 @@ export 'package:flutter_components/screens/alert_screen.dart';
 export 'package:flutter_components/screens/card_screen.dart';
 export 'package:flutter_components/screens/listView1_screen.dart';
 export 'package:flutter_components/screens/listView2_screen.dart';
-
-
+export "package:flutter_components/screens/input_screen.dart";
