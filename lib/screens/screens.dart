53fb56c0defb30ae31bbfbf675c2272514188e87
jerlibgnzlz <jerlibgv@gmail.com>
2023-06-25 02:47:20 -0300

    widget scrollInfinity

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,3 +1,6 @@
+
+
+export 'package:flutter_components/screens/listview_builder_screen.dart';
 export 'package:flutter_components/screens/slider_screen.dart';
 export 'package:flutter_components/screens/animated_screen.dart';
 export 'package:flutter_components/screens/avatar_screen.dart';
@@ -7,4 +10,5 @@ export 'package:flutter_components/screens/card_screen.dart';
 export 'package:flutter_components/screens/listView1_screen.dart';
 export 'package:flutter_components/screens/listView2_screen.dart';
 export "package:flutter_components/screens/input_screen.dart";
+
 
--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -9,39 +9,71 @@ class SliderScreen extends StatefulWidget {
 }
 
 class _SliderScreenState extends State<SliderScreen> {
+
   double _sliderValue = 100;
+
+  bool _sliderCheck = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Slider & ckecks"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-           children: [
-            Slider(
-              activeColor: AppThemes.primary,
-              inactiveColor: Colors.blueAccent,
-              min: 60,
-              max: 400,
-              value: _sliderValue,
-              onChanged: (value) {
-                setState(() {
-                  _sliderValue = value;
-                });
-              },
+      body: Column(children: [
+        Slider(
+          activeColor: AppThemes.primary,
+          inactiveColor: Colors.blueAccent,
+          min: 60,
+          max: 400,
+          value: _sliderValue,
+          onChanged:_sliderCheck ? (value) {
+            setState(() {
+              _sliderValue = value;
+            });
+          }: null,
+        ),
+        CheckboxListTile.adaptive(
+          title: const Text("Habilitar y desabilitar la imagen"),
+          checkColor: Colors.red,
+          side:const BorderSide(color: Colors.green),
+          value: _sliderCheck,
+           onChanged: (value) {
+             setState(() {
+                 _sliderCheck = value ?? false;
+             });
+           },),
+
+        SwitchListTile(
+            title: const Text("Habilitar Slider"),
+            activeColor: Colors.yellowAccent,
+            activeTrackColor: AppThemes.primary,
+            value: _sliderCheck,
+            onChanged: (value) {
+              setState(() {
+                 _sliderCheck = value;
+              });
+            },),
+
+            const AboutListTile(
+            icon: Icon(Icons.info_outline_rounded),
+         
+            
             ),
-      
-             Image(
-              image:const NetworkImage(
+
+
+        Expanded(
+          child: SingleChildScrollView(
+            child: Image(
+              image: const NetworkImage(
                   "https://w7.pngwing.com/pngs/723/728/png-transparent-iron-man-iron-man-s-superhero-fictional-character-film.png"),
               fit: BoxFit.fill,
-               width:_sliderValue,
+              width: _sliderValue,
             ),
-           const SizedBox(width: 100)
-          ]),
-      ),
-      
+          ),
+        ),
+        const SizedBox(width: 100)
+      ]),
     );
   }
 }
